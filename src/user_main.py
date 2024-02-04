4c72679ac558f7644d572986eba36ea83e828a25
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-02-04 11:04:40 +0000

    feat: crude centralised user script runner.

---
+++ src/user_main.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def main():
+    # subprocess.run(['python3', 'gui/LookBetween.py', '00'])
+    # subprocess.run(['python3', 'configs/set_batch_configs.py'])
+    # subprocess.run(['python3', 'spectrogram/set_background_vector.py', '03'])
+    # subprocess.run(['bash', 'run_observations/batch/batch_observe.sh', '2', '10', '03'])
+    pass
+
+if __name__ == "__main__":
+    main()
