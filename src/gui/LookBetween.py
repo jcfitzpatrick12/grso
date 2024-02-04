aaa244167f6c9aebe474aff348816fab7af1d364
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-02-04 18:32:25 +0000

    chore: code re-arranging

--- src/gui/LookBetween.py
+++ src/gui/LookBetween.py
@@ -20,6 +20,7 @@ class LookBetween:
         self.setup_widgets()
         self.update_tag_and_chunks(default_tag)
 
+
     def setup_widgets(self):
         self.tag_entry = self.create_entry("Tag: ", self.tag, 0)
         update_tag_button = tk.Button(self.master, text="Update Tag", command=lambda: self.update_tag_and_chunks(self.tag_entry.get()))
@@ -35,12 +36,30 @@ class LookBetween:
         self.canvas = FigureCanvasTkAgg(self.figure, master=self.master)
         self.canvas.get_tk_widget().grid(row=9, column=0, columnspan=3)
 
+    
+    def create_entry(self, label, default, row):
+        tk.Label(self.master, text=label).grid(row=row, column=0)
+        entry = tk.Entry(self.master)
+        entry.grid(row=row, column=1)
+        entry.insert(0, default)
+        return entry
+
+
+    def create_checkboxes(self, label, plot_types, row):
+        label_frame = tk.LabelFrame(self.master, text=label)
+        label_frame.grid(row=row, column=0, columnspan=3)
+        for plot_type in plot_types:
+            check = tk.Checkbutton(label_frame, text=plot_type, variable=self.plot_type_vars[plot_type])
+            check.pack(side=tk.LEFT)
+
+
     def update_tag_and_chunks(self, new_tag):
         self.tag = new_tag
         self.chunks = Chunks(new_tag)
         self.set_default_values()
         self.update_entry_fields()
         self.draw_plot_types()
+
 
     def set_default_values(self):
         default_chunk = self.chunks.get_chunk_by_index(-1)
@@ -50,8 +69,8 @@ class LookBetween:
             'end': datetime.strftime(default_S.datetime_array[-1], GLOBAL_CONFIG.default_time_format),
             'lower_freq': round(default_S.freqs_MHz[0], 2),
             'upper_freq': round(default_S.freqs_MHz[-1], 2),
-            'time_avg': 1,
-            'freq_avg': 1
+            'avg_over_int_time': 1,
+            'avg_over_int_freq': 1
         }
 
     def get_field_titles_dict(self):
@@ -61,18 +80,20 @@ class LookBetween:
             'end': "End time: ",
             'lower_freq': "Lower frequency [MHz]: ",
             'upper_freq': "Higher frequency [MHz]: ",
-            'time_avg': "Average over int (time): ",
-            'freq_avg': "Average over int (frequency): ",
+            'avg_over_int_time': "Average over int (time): ",
+            'avg_over_int_freq': "Average over int (frequency): ",
         }
 
         return field_titles_dict
 
+
     def update_entry_fields(self):
         self.entries = {}
-        fields = ['start', 'end', 'lower_freq', 'upper_freq', 'time_avg', 'freq_avg']
+        fields = ['start', 'end', 'lower_freq', 'upper_freq', 'avg_over_int_time', 'avg_over_int_freq']
         field_titles_dict = self.get_field_titles_dict()
         for i, field in enumerate(fields, start=1):
             self.entries[field] = self.create_entry(field_titles_dict[field], self.default_values[field], i)
+
 
     def draw_plot_types(self):
         # Clear previous checkboxes if they exist
@@ -90,28 +111,22 @@ class LookBetween:
             check = tk.Checkbutton(self.plot_type_frame, text=plot_type, variable=self.plot_type_vars[plot_type])
             check.pack(side=tk.LEFT)
 
-    def create_entry(self, label, default, row):
-        tk.Label(self.master, text=label).grid(row=row, column=0)
-        entry = tk.Entry(self.master)
-        entry.grid(row=row, column=1)
-        entry.insert(0, default)
-        return entry
+    
+    def get_selected_plot_types(self):
+        """Retrieve a list of selected plot types based on the checkboxes."""
+        return [plot_type for plot_type, var in self.plot_type_vars.items() if var.get() == True]
 
-    def create_checkboxes(self, label, plot_types, row):
-        label_frame = tk.LabelFrame(self.master, text=label)
-        label_frame.grid(row=row, column=0, columnspan=3)
-        for plot_type in plot_types:
-            check = tk.Checkbutton(label_frame, text=plot_type, variable=self.plot_type_vars[plot_type])
-            check.pack(side=tk.LEFT)
 
     def get_spectrogram(self):
         # Input validation and error handling can be added here
         start_str = self.entries['start'].get()
         end_str = self.entries['end'].get()
+        S = self.chunks.build_spectrogram_from_range(start_str, end_str)
         lower_freq = float(self.entries['lower_freq'].get())
         upper_freq = float(self.entries['upper_freq'].get())
-        S = self.chunks.build_spectrogram_from_range(start_str, end_str)
-        return SpectrogramFactory.frequency_chop(S, lower_freq, upper_freq)
+        S = SpectrogramFactory.frequency_chop(S, lower_freq, upper_freq)
+        return S
+
 
     def plot_data(self):
         self.figure.clear()
@@ -126,11 +141,8 @@ class LookBetween:
         selected_plot_types = self.get_selected_plot_types()
         if selected_plot_types:
             S.stack_plots(self.figure, selected_plot_types)
-        self.canvas.draw()
 
-    def get_selected_plot_types(self):
-        """Retrieve a list of selected plot types based on the checkboxes."""
-        return [plot_type for plot_type, var in self.plot_type_vars.items() if var.get() == True]
+        self.canvas.draw()
 
 
     def save_figure_as_pdf(self):
@@ -139,8 +151,10 @@ class LookBetween:
         save_path = os.path.join(GLOBAL_CONFIG.path_to_figures, f"figure_{self.tag}")
         self.figure.savefig(save_path, format='pdf')
 
+
     def exit_application(self):
         self.master.destroy()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
