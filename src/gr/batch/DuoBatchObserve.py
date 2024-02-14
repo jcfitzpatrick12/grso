049279a31efe72a097aa164178dde8acf8bba7f7
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-02-14 21:32:21 +0000

    feat: crude dual observations with RSPDuo

--- src/gr/batch/DuoBatchObserve.py
+++ src/gr/batch/DuoBatchObserve.py
@@ -20,7 +20,6 @@ if __name__ == '__main__':
         except:
             print("Warning: failed to XInitThreads()")
 
-from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
@@ -32,16 +31,20 @@ from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import gr, blocks
 from gnuradio import sdrplay3
-from gnuradio import qtgui
 import pmt
 
 from src.configs import GLOBAL_CONFIG
 from src.configs.JsonConfig import load_config
 from src.gr.batch.MetaDict import MetaDict
-from src.gr.batch import TimeStamper 
+from src.gr.batch import TimeStamper  
 
-class BatchObserve(gr.top_block, Qt.QWidget):
-    def __init__(self, tag):
+
+
+from gnuradio import qtgui
+
+class DuoBatchObserve(gr.top_block, Qt.QWidget):
+
+    def __init__(self, tags):
         gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
         Qt.QWidget.__init__(self)
         self.setWindowTitle("Collecting data.")
@@ -62,7 +65,7 @@ class BatchObserve(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "BatchObserve")
+        self.settings = Qt.QSettings("GNU Radio", "DuoBatchObserve")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -76,59 +79,86 @@ class BatchObserve(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
 
-        config_dict = load_config("batch", tag)
-        self.samp_rate = config_dict['samp_rate']
-        self.IF_gain = config_dict['IF_gain']
-        self.RF_gain = config_dict['RF_gain']
-        self.center_freq = config_dict['center_freq']
+        config_dict_0 = load_config("batch", tags[0])
+        config_dict_1 = load_config("batch", tags[1])
+
+        self.samp_rate_0 = config_dict_0['samp_rate']
+        self.samp_rate_1 = config_dict_1['samp_rate']
+
+        if self.samp_rate_0 != self.samp_rate_1:
+            raise ValueError(f"Sample rates must be equal. samp_rate_0: {samp_rate_0}, samp_rate_1: {samp_rate_1}")
+
+        self.samp_rate = self.samp_rate_0
+
+        self.IF_gain_0 = config_dict_0 ['IF_gain']
+        self.IF_gain_1 = config_dict_1['IF_gain']
+    
+
+        self.RF_gain_0 = config_dict_0['RF_gain']
+        self.RF_gain_1 = config_dict_1['RF_gain']
+
+
+        self.center_freq_0 = config_dict_0['center_freq']
+        self.center_freq_1 = config_dict_1['center_freq']
+
         self.chunk_start_time = TimeStamper.return_time_now_as_string()
 
         ##################################################
         # Blocks
         ##################################################
-        self.sdrplay3_rsp1a_0 = sdrplay3.rsp1a(
+        self.sdrplay3_rspduo_0 = sdrplay3.rspduo(
             '',
+            rspduo_mode="Dual Tuner (independent RX)",
+            antenna="Both Tuners",
             stream_args=sdrplay3.stream_args(
                 output_type='fc32',
-                channels_size=1
+                channels_size=2
             ),
         )
-        self.sdrplay3_rsp1a_0.set_sample_rate(self.samp_rate)
-        self.sdrplay3_rsp1a_0.set_center_freq(self.center_freq)
-        self.sdrplay3_rsp1a_0.set_bandwidth(self.samp_rate)
-        self.sdrplay3_rsp1a_0.set_gain_mode(False)
-        self.sdrplay3_rsp1a_0.set_gain(self.IF_gain, 'IF')
-        self.sdrplay3_rsp1a_0.set_gain(self.RF_gain, 'RF')
-        self.sdrplay3_rsp1a_0.set_freq_corr(0)
-        self.sdrplay3_rsp1a_0.set_dc_offset_mode(False)
-        self.sdrplay3_rsp1a_0.set_iq_balance_mode(False)
-        self.sdrplay3_rsp1a_0.set_agc_setpoint(-30)
-        self.sdrplay3_rsp1a_0.set_rf_notch_filter(False)
-        self.sdrplay3_rsp1a_0.set_dab_notch_filter(False)
-        self.sdrplay3_rsp1a_0.set_biasT(False)
-        self.sdrplay3_rsp1a_0.set_debug_mode(False)
-        self.sdrplay3_rsp1a_0.set_sample_sequence_gaps_check(False)
-        self.sdrplay3_rsp1a_0.set_show_gain_changes(False)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, self.samp_rate,True)
+        self.sdrplay3_rspduo_0.set_sample_rate(self.samp_rate)
+        self.sdrplay3_rspduo_0.set_center_freq(self.center_freq_0, self.center_freq_1)
+        self.sdrplay3_rspduo_0.set_bandwidth(self.samp_rate)
+        self.sdrplay3_rspduo_0.set_antenna("Both Tuners")
+        self.sdrplay3_rspduo_0.set_gain_modes(False, False)
+        self.sdrplay3_rspduo_0.set_gain(self.IF_gain_0, self.IF_gain_1, 'IF')
+        self.sdrplay3_rspduo_0.set_gain(self.RF_gain_0, self.RF_gain_1, 'RF')
+        self.sdrplay3_rspduo_0.set_freq_corr(0)
+        self.sdrplay3_rspduo_0.set_dc_offset_mode(False)
+        self.sdrplay3_rspduo_0.set_iq_balance_mode(False)
+        self.sdrplay3_rspduo_0.set_agc_setpoint(-30)
+        self.sdrplay3_rspduo_0.set_rf_notch_filter(False)
+        self.sdrplay3_rspduo_0.set_dab_notch_filter(False)
+        self.sdrplay3_rspduo_0.set_am_notch_filter(False)
+        self.sdrplay3_rspduo_0.set_biasT(False)
+        self.sdrplay3_rspduo_0.set_debug_mode(False)
+        self.sdrplay3_rspduo_0.set_sample_sequence_gaps_check(False)
+        self.sdrplay3_rspduo_0.set_show_gain_changes(False)
 
-        temp_file_path = TimeStamper.return_temp_file_path(self.chunk_start_time, tag)
+
+        temp_file_path_0 = TimeStamper.return_temp_file_path(self.chunk_start_time, tags[0])
+        temp_file_path_1 = TimeStamper.return_temp_file_path(self.chunk_start_time, tags[1])
         
-        md = MetaDict()
-        meta_dict = md.get_dict(self.samp_rate,self.center_freq,self.chunk_start_time)
+        md_0 = MetaDict()
+        meta_dict_0 = md_0.get_dict(self.samp_rate,self.center_freq_0,self.chunk_start_time)
+        self.blocks_file_meta_sink_0 = blocks.file_meta_sink(gr.sizeof_gr_complex*1, temp_file_path_0, self.samp_rate, 1, blocks.GR_FILE_FLOAT, True, 1000000, meta_dict_0, True)
+        self.blocks_file_meta_sink_0.set_unbuffered(False)
 
-        self.blocks_file_meta_sink_0_0 = blocks.file_meta_sink(gr.sizeof_gr_complex*1, temp_file_path, self.samp_rate, 1, blocks.GR_FILE_FLOAT, True, 1000000, meta_dict, True)
-        self.blocks_file_meta_sink_0_0.set_unbuffered(False)
+        md_1 = MetaDict()
+        meta_dict_1 = md_0.get_dict(self.samp_rate,self.center_freq_1,self.chunk_start_time)
+        self.blocks_file_meta_sink_1 = blocks.file_meta_sink(gr.sizeof_gr_complex*1, temp_file_path_1, self.samp_rate, 1, blocks.GR_FILE_FLOAT, True, 1000000, meta_dict_1, True)
+        self.blocks_file_meta_sink_1.set_unbuffered(False)
+
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_file_meta_sink_0_0, 0))
-        self.connect((self.sdrplay3_rsp1a_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.sdrplay3_rspduo_0, 0), (self.blocks_file_meta_sink_0, 0))
+        self.connect((self.sdrplay3_rspduo_0, 1), (self.blocks_file_meta_sink_1, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "BatchObserve")
+        self.settings = Qt.QSettings("GNU Radio", "rsp_duo_test")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -137,7 +167,8 @@ class BatchObserve(gr.top_block, Qt.QWidget):
 
 
 
-def main(tag, top_block_cls=BatchObserve, options=None):
+
+def main(tags, top_block_cls=DuoBatchObserve, options=None):
 
     '''
     # Parsing command line arguments
@@ -156,7 +187,7 @@ def main(tag, top_block_cls=BatchObserve, options=None):
         Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
-    tb = top_block_cls(tag)
+    tb = top_block_cls(tags)
 
     tb.start()
 
@@ -179,11 +210,14 @@ def main(tag, top_block_cls=BatchObserve, options=None):
 
 if __name__ == '__main__':
     try:
-        tag = str(sys.argv[1])
+        tags_int = sys.argv[1:]
     except:
         raise ValueError("Please specify the tag by passing in through the command line.")
-    
-    if tag not in GLOBAL_CONFIG.defined_tags:
-        raise ValueError(f"Please specify a valid tag. Received {tag}, need one of {GLOBAL_CONFIG.defined_tags}")
 
-    main(tag)
+    tags = []
+    for tag in tags_int:
+        if tag not in GLOBAL_CONFIG.defined_tags:
+            raise ValueError(f"Please specify a valid tag. Received {tag}, need one of {GLOBAL_CONFIG.defined_tags}")
+        tags.append(str(tag))
+    
+    main(tags)
---
+++ src/run_observations/batch/duo_batch_observe.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+
+#run the code saving data for sleepy_time seconds, max_iter times
+max_iter=$1
+sleepy_time=$2
+tag_0=$3
+tag_1=$4 
+
+cd "$GRSOPARENTPATH"
+
+#create the temporary data folder if it does not exist
+bash src/shell_utils/create_dir.sh "temp_data_${tag_0}"
+#create the temporary data folder if it does not exist
+bash src/shell_utils/create_dir.sh "temp_data_${tag_1}"
+
+#build the data path if it does not already exist
+date_dir=$(date +"%Y/%m/%d")
+data_dir="data/$date_dir"
+bash src/shell_utils/create_dir.sh $data_dir
+
+
+temp_path_0="${GRSOPARENTPATH}/temp_data_${tag_0}"
+temp_path_1="${GRSOPARENTPATH}/temp_data_${tag_1}"
+data_path="$GRSOPARENTPATH/$data_dir"
+
+
+for (( i=1; i<=max_iter; i=i+1 ))
+do
+  # Inform the user of which iteration we are on
+  echo "Running iteration $i"
+  
+  # Start the Python script as a background process, then kill it after sleepy_time seconds
+  python3 src/gr/batch/DuoBatchObserve.py $tag_0 $tag_1 &
+
+  #capture the processing id of the script
+  pid=$!
+
+  #sleep for the determined amount of seconds
+  sleep ${sleepy_time}s
+
+  #kill the observation
+  kill $pid
+
+  # Move all files from the source to the destination directory
+  mv "$temp_path_0"/* "$data_path"/
+  # Move all files from the source to the destination directory
+  mv "$temp_path_1"/* "$data_path"/
+
+  #run the postprocessing as a background script if we have another observational cycle to run
+  if (( $i < $max_iter )); then 
+    python3 src/run_observations/batch/duo_proc_batch.py $tag_0 $tag_1 &
+  else
+  #otherwise, just run as a foreground process so that the script can end gracefully
+    python3 src/run_observations/batch/duo_proc_batch.py $tag_0 $tag_1 
+  fi
+
+  #give the script time to stop, so the SDR isnt busy when the new script starts
+  # If you give it too much time, there will be deadtime at the start of the spectrograms!
+  sleep 0.5s
+
+done
+
+## clean-up
+rm -r $temp_path_0
+rm -r $temp_path_1
+
+
+
